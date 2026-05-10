28633707a8a66cbb4b3e53b2c7eaf23d1982576a
巫孟謙 <@DESKTOP-BA1APLI>
2026-05-10 15:08:22 +0800

    加入專案檔案。

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MediaPlayer
+{
+    partial class frmMediaPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMediaPlayer));
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.palButton = new System.Windows.Forms.Panel();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.palButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.Location = new System.Drawing.Point(42, 28);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(81, 60);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Enabled = false;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.Location = new System.Drawing.Point(159, 28);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 60);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.Location = new System.Drawing.Point(278, 28);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 54);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.Location = new System.Drawing.Point(399, 28);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 54);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // palButton
+            // 
+            this.palButton.Controls.Add(this.btnBrowser);
+            this.palButton.Controls.Add(this.btnStop);
+            this.palButton.Controls.Add(this.btnPlay);
+            this.palButton.Controls.Add(this.btnPause);
+            this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.palButton.Location = new System.Drawing.Point(0, 355);
+            this.palButton.Name = "palButton";
+            this.palButton.Size = new System.Drawing.Size(550, 100);
+            this.palButton.TabIndex = 5;
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(550, 455);
+            this.wmpVideo.TabIndex = 0;
+            // 
+            // frmMediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 455);
+            this.Controls.Add(this.palButton);
+            this.Controls.Add(this.wmpVideo);
+            this.Name = "frmMediaPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "多媒體播放器";
+            this.palButton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Panel palButton;
+    }
+}
+
